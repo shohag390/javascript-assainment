88580ab038abb80020b461d29a4e44a5e7c5df43
shohag390 <shohag390@users.noreply.github.com>
2025-02-17 20:04:36 +0600

    Folder Create

---
+++ script.js
@@ -0,0 +1,38 @@
+/*function signature/sample */
+
+function calculateVAT(price) {
+    // You have to write your code here
+
+}
+
+
+
+/*function signature/sample */
+function validContact(contact) {
+    // You have to write your code here
+
+}
+
+
+
+/*function signature/sample */
+function willSuccess(marks) {
+    // You have to write your code here
+
+}
+
+
+/*function signature/sample */
+function validProposal(person1, person2) {
+    // You have to write your code here
+
+}
+
+
+/*function signature/sample */
+function calculateSleepTime(times) {
+    // You have to write your code here
+
+}
+
+
